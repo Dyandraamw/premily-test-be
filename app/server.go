f944b4046b6d769e27f28181cb44de619e62ad4a
unknown <ndrurufrangklynbf@gmail.com>
2024-04-12 17:00:46 +0700

    setting koneksi db & env

--- app/server.go
+++ app/server.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -15,8 +18,34 @@ type Server struct {
 
 }
 
-func (server *Server) Initialize(){
-	fmt.Println("selamat datang di Premily backend")
+type AppConfig struct {
+	AppName string
+	AppEnv	string
+	AppPort	string
+}
+
+type DBConfig struct{
+	DBHost	string
+	DBUser	string
+	DBPassword	string
+	DBName		string
+	DBPort		string
+}
+
+func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig){
+	fmt.Println("selamat datang di " + appConfig.AppName)
+
+
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+						dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword,
+							dbConfig.DBName, dbConfig.DBPort)
+	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+		// fmt.Printf("Gagal melakukan koneksi ke database!")
+	}
 
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
@@ -28,9 +57,38 @@ func (server *Server) Run(addr string){
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
 
+
+//method yang digunakan untuk menjaga apabila ENV kosong
+/*func getEnv(key, fallback string)string{ 
+	if value, ok := os.LookupEnv(key); ok{
+		return value
+	}
+
+	return fallback
+}*/
+//getEnv[namamethod]("value dari ENV", "value dari getEnv[default]")
+
 func Run(){
 	var server = Server{}
+	var appConfig	= AppConfig{}
+	var dbConfig	= DBConfig{}
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("Error OS loading .env file")
+	}
 
-	server.Initialize()
-	server.Run(":9090")
+
+
+	appConfig.AppName =os.Getenv("APP_NAME")
+	appConfig.AppEnv  = os.Getenv("APP_ENV")
+	appConfig.AppPort = os.Getenv("APP_PORT")
+
+	dbConfig.DBHost		= os.Getenv("DB_HOST")
+	dbConfig.DBUser		= os.Getenv("DB_USER")
+	dbConfig.DBPassword		= os.Getenv("DB_PASSWORD")
+	dbConfig.DBName		= os.Getenv("DB_NAME")
+	dbConfig.DBPort		= os.Getenv("DB_PORT")
+
+	server.Initialize(appConfig, dbConfig)
+	server.Run(":"+ appConfig.AppPort)
 }
