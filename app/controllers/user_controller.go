290c252926d2416c0c849fb4c7f4c23938881ecb
unknown <ndrurufrangklynbf@gmail.com>
2024-06-05 03:29:51 +0700

    payment status done, remember me done, completed field invoice and user done

--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -40,6 +40,7 @@ func (server *Server) GetUserAction(w http.ResponseWriter, r *http.Request) {
 	}
 
 	userDetails := struct {
+		Image       string      `json:image`
 		Username    string      `json:"username"`
 		Name        string      `json:"name"`
 		Email       string      `json:"email"`
@@ -47,6 +48,7 @@ func (server *Server) GetUserAction(w http.ResponseWriter, r *http.Request) {
 		CompanyName string      `json:"company_name"`
 		Role        models.Role `json:"role"`
 	}{
+		Image:       user.Image,
 		Username:    user.Username,
 		Name:        user.Name,
 		Email:       user.Email,
@@ -144,37 +146,37 @@ func (server *Server) SignUpAction(w http.ResponseWriter, r *http.Request) {
 	}
 
 	file, handler, err := r.FormFile("image")
-    if err != nil {
-        http.Error(w, "Failed to get image: "+err.Error(), http.StatusBadRequest)
-        return
-    }
-    defer file.Close()
+	if err != nil {
+		http.Error(w, "Failed to get image: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
 
-    // Create a directory to save the uploaded files
-    uploadDir := "./uploads"
-    if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
-        os.Mkdir(uploadDir, os.ModePerm)
-    }
+	// Create a directory to save the uploaded files
+	uploadDir := "./uploads"
+	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+		os.Mkdir(uploadDir, os.ModePerm)
+	}
 
-    // Create a new file in the uploads directory
-    filePath := filepath.Join(uploadDir, handler.Filename)
-    dst, err := os.Create(filePath)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    defer dst.Close()
+	// Create a new file in the uploads directory
+	filePath := filepath.Join(uploadDir, handler.Filename)
+	dst, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
 
-    // Copy the uploaded file to the new file
-    if _, err := file.Seek(0, 0); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    if _, err := dst.ReadFrom(file); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-	 
+	// Copy the uploaded file to the new file
+	if _, err := file.Seek(0, 0); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if _, err := dst.ReadFrom(file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	if err := ValidatePassword(password); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
